86b2f392ad9a4ff1ddf38e2b880531da20ed0f8f
luiznai <luiznai@br.ibm.com>
2019-04-08 15:26:10 -0300

    Update DB modules.

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+var connMySQL = function(){
+  console.log('Conexao com bd foi estabelecida');
+  return mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '1234',
+      database: 'portal_noticias'
+  });
+}
+
+
 module.exports = function (){
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'portal_noticias'
-    });
+  console.log('O autoload carregou o modulo de conexao com o bd');
+  return connMySQL;
 }
